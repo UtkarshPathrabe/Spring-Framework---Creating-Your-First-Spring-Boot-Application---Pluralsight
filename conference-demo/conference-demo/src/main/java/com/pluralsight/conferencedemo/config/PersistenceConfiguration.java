071af47ad5afe12933820c38f9aee8d659a3545a
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-12-22 13:41:25 +0530

    Update PersistenceConfiguration.java

--- conference-demo/conference-demo/src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ conference-demo/conference-demo/src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -1,6 +1,5 @@
 package com.pluralsight.conferencedemo.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,22 +9,12 @@ import javax.sql.DataSource;
 @Configuration
 public class PersistenceConfiguration {
 
-    @Value("${spring.datasource.url}")
-    private String dataSourceURL;
-
-    @Value("${spring.datasource.username}")
-    private String dataSourceUserName;
-
-    @Value("${spring.datasource.password}")
-    private String dataSourcePassword;
-
     @Bean
     public DataSource dataSource() {
         DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url(dataSourceURL);
-        builder.username(dataSourceUserName);
-        builder.password(dataSourcePassword);
-        System.out.println("My custom data-source bean has been initialized and set");
+        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+        builder.username("postgres");
+        builder.password("Welcome");
         return builder.build();
     }
 
